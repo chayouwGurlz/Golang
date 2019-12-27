93c037a88f04b769e5f0f48da6405a8ec8c6dc79
Irine Octaviana <irine.octaviana@gameloft.com>
2019-12-27 16:29:52 +0700

    + add variables and comments

--- test/test.go
+++ test/test.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main(){
 	fmt.Println("Hello world")
+	fmt.Println("2nd Hello world")
+	
+	var val1 = 4
+	var Val1 = 9
+	/*var val2 = 6 + 8
+	var val3 = 89*/
+	
+	fmt.Println(val1)
+	fmt.Println(Val1)
+	//fmt.Println(val2)
 }
