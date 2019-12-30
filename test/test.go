7f423a56691954460312e4e47eb65c4c274dcdc9
Irine Octaviana <irine.octaviana@gameloft.com>
2019-12-30 09:33:30 +0700

    - check datatype on variable

--- test/test.go
+++ test/test.go
@@ -10,8 +10,14 @@ func main(){
 	var Val1 = 9
 	/*var val2 = 6 + 8
 	var val3 = 89*/
+	var var3 = 5
+	var var4 = 5.34
+	
 	
 	fmt.Println(val1)
 	fmt.Println(Val1)
 	//fmt.Println(val2)
+	fmt.Println(Val1 + val1)
+	fmt.Printf("%T\n", var3) //check datatype
+	fmt.Printf("%T\n", var4)
 }
