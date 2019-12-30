2f787c181a324f2e22df3158b7d0d146e01dcb47
Irine Octaviana <irine.octaviana@gameloft.com>
2019-12-30 10:27:13 +0700

    - advantage of Go variables

--- test/test.go
+++ test/test.go
@@ -10,14 +10,26 @@ func main(){
 	var Val1 = 9
 	/*var val2 = 6 + 8
 	var val3 = 89*/
-	var var3 = 5
-	var var4 = 5.34
+	var val3 = 5
+	var val5 int = 6
+	var val2 int
+	var val4 = 5.34
+	var val6 float64 = 5.89
+	var val7 float64
+	var val8, val9, val10 = "foo", 8, 3.14
 	
 	
 	fmt.Println(val1)
 	fmt.Println(Val1)
 	//fmt.Println(val2)
 	fmt.Println(Val1 + val1)
-	fmt.Printf("%T\n", var3) //check datatype
-	fmt.Printf("%T\n", var4)
+	fmt.Println(val2)
+	fmt.Println(val7)
+	fmt.Println(val8)
+	fmt.Println(val9)
+	fmt.Println(val10)
+	fmt.Printf("%T\n", val3) //check datatype
+	fmt.Printf("%T\n", val5)
+	fmt.Printf("%T\n", val4)
+	fmt.Printf("%T\n", val6)
 }
